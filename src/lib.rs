dcb1b0fefc322db40b74c6abe3b14723fe02fe37
Yolier Galán Tassé <gallegogt@gmail.com>
2020-05-17 11:00:09 -0400

    Upsate Sample module

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@
 pub mod transport;
 
 pub mod sample {
+    use crate::transport::payload::Payload;
     use chrono::{Local, NaiveDateTime};
+    use iota_streams::{app_channels::api::tangle::DefaultTW, protobuf3::types::Trytes};
     use rand::Rng;
     use serde::{Deserialize, Serialize};
 
@@ -13,18 +15,26 @@ pub mod sample {
     ///
     #[derive(Debug, Serialize, Deserialize)]
     pub struct StreamsData {
+        /// Current Timestap
         ts: NaiveDateTime,
-        data: String,
+        /// Sample description
+        desc: String,
+        /// Temperature
+        temperature: f32,
+        /// Pressure
+        pressure: f32,
     }
 
     impl StreamsData {
-        pub fn new<S>(data: S) -> Self
+        pub fn new<S>(desc: S, temp: f32, pressure: f32) -> Self
         where
             S: Into<String>,
         {
             StreamsData {
                 ts: Local::now().naive_utc(),
-                data: data.into(),
+                desc: desc.into(),
+                temperature: temp,
+                pressure: pressure,
             }
         }
     }
@@ -32,7 +42,11 @@ pub mod sample {
     impl Default for StreamsData {
         fn default() -> Self {
             let mut rng = rand::thread_rng();
-            StreamsData::new(make_random_data(rng.gen_range(10, 200)))
+            StreamsData::new(
+                make_random_data(rng.gen_range(10, 50)),
+                rng.gen_range(-10.0, 1.3e3),
+                rng.gen_range(10.0, 1.3e5),
+            )
         }
     }
 
@@ -42,16 +56,36 @@ pub mod sample {
     pub(crate) fn make_random_data(len: usize) -> String {
         const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
                                 abcdefghijklmnopqrstuvwxyz\
-                                0123456789)(*&^%$#@!~;,.";
+                                0123456789 ;,.";
         let mut rng = rand::thread_rng();
 
-        let password: String = (0..len)
+        let rand_string: String = (0..len)
             .map(|_| {
                 let idx = rng.gen_range(0, CHARSET.len());
                 CHARSET[idx] as char
             })
             .collect();
-        password
+        rand_string
+    }
+
+    /// Print message payload
+    ///
+    pub fn print_message_payload<T>(prefix: T, public: Trytes<DefaultTW>, masked: Trytes<DefaultTW>)
+    where
+        T: Into<String>,
+    {
+        let p_data: Option<StreamsData> = Payload::unwrap_data(&public).unwrap();
+        let m_data: Option<StreamsData> = Payload::unwrap_data(&masked).unwrap();
+        let pfx = prefix.into();
+
+        match p_data {
+            Some(d) => println!("\n {} Public Packet: \n \t{:?}\n", pfx, d),
+            None => {}
+        }
+        match m_data {
+            Some(d) => println!("\n {} Masked Packet: \n \t{:?}\n", pfx, d),
+            None => {}
+        }
     }
 }
 
